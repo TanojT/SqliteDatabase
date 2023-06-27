b12bd0497d1fcb91460cc8389f1c5a81d9013f77
unknown <ttelikepalli@gmail.com>
2023-06-27 17:22:57 -0500

    Sqlite3 database Initial Commit

---
+++ src/main/java/com/example/dataBaseCreate.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import java.sql.*;
+
+public class dataBaseCreate {
+    public static void main(String[] args){
+        Connection connect = null;
+        Statement stm = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connect = DriverManager.getConnection("jdbc:sqlite:test.db");
+            System.out.println("Opened Database Successfully");
+
+            stm=connect.createStatement();
+            String sql = "CREATE TABLE EMPLOYEE"+
+                            "(ID INT PRIMARY KEY     NOT NULL,"+
+                            "NAME           TEXT    NOT NULL,"+
+                            "AGE            INT     NOT NULL,"+
+                            "JOB DESCRIPTION TEXT   NOT NULL,"+
+                            "SALARY         REAL)";
+            stm.executeUpdate(sql);
+            stm.close();
+            connect.close();
+
+        } catch (Exception e) {
+            
+            e.printStackTrace();
+        } 
+        System.out.println("Table created Successfully");
+        
+    }
+}
---
+++ src/main/java/com/example/dataBaseInsert.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import java.sql.*;
+
+public class dataBaseInsert {
+    
+    public static void main(String[] args){
+        Connection connect = null;
+        Statement stm = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connect =DriverManager.getConnection("jdbc:sqlite:test.db");
+            connect.setAutoCommit(false);
+            System.out.println("Opened Database !");
+            stm= connect.createStatement();
+            String sql ="INSERT INTO EMPLOYEE(ID,NAME,AGE,[JOB],SALARY)"+
+                        "vALUES(1002,'Pandu',25,'DALLAS',40000.00)";
+            stm.executeUpdate(sql);
+            sql ="INSERT INTO EMPLOYEE(ID,NAME,AGE,[JOB],SALARY)"+
+                        "vALUES(1003,'Subbu',20,'Kansas',45000.00)";
+            stm.executeUpdate(sql);
+            sql ="INSERT INTO EMPLOYEE(ID,NAME,AGE,[JOB],SALARY)"+
+                        "vALUES(1004,'pranathi',28,'DALLAS',20000.00)";
+            stm.executeUpdate(sql);
+            sql ="INSERT INTO EMPLOYEE(ID,NAME,AGE,[JOB],SALARY)"+
+                        "vALUES(1005,'priyanka',27,'Austin',35000.00)";
+            stm.executeUpdate(sql);
+            stm.close();
+            connect.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+}
